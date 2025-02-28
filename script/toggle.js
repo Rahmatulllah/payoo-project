ff6405e6aac9ba04a2b047f0200c58736ee53557
Rahmatullah <hrmilon1997@gmail.com>
2025-02-28 20:07:39 +0600

    my second commit

---
+++ script/toggle.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById('cash-out').style.display = "none";
+
+document.getElementById('addmoney-box').addEventListener('click',function(){
+   document.getElementById('cash-out').style.display = "none";
+   document.getElementById('add-money').style.display = "block";
+})
+
+document.getElementById('cashout-box').addEventListener('click',function(){
+   document.getElementById('cash-out').style.display = "block";
+   document.getElementById('add-money').style.display = "none";
+})
